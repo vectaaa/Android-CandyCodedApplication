87ac8d695308bdea384868c2a6b15436e29e38de
Victor Onuchukwu <chisomonuchukwu91@gmail.com>
2020-10-15 07:45:21 +0100

    new

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -61,6 +62,7 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -71,4 +73,16 @@ public class DetailActivity extends AppCompatActivity {
     // ***
     // TODO - Task 4 - Share the Current Candy with an Intent
     // ***
+    private void createShareIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -26,8 +28,26 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        Uri mapURI = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapURI);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapURI != null){
+            mapIntent.resolveActivity(getPackageManager());
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri phoneURI = Uri.parse("tel:0123456789");
+
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(phoneURI);
+        startActivity(phoneIntent);
+    }
 }
